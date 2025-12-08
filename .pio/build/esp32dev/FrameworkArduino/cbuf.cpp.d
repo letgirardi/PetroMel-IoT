ea112d5e3af1ce7893b7195242adeb2711c7486d
Deyvid Barcelos <bardeyvid@gmail.com>
2025-12-08 10:06:52 -0300

    feat: change on the dashboard metrics

--- .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/deyvid_barcelos/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/deyvid_barcelos/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
